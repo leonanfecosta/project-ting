d15fe70711de101082b863bc593cbf2055ca267a
Leonan Costa <leonanfecosta@gmail.com>
2023-02-04 03:25:34 -0300

    feat:  Implemente a função process capaz de transformar o conteúdo da lista gerada em um dicionário que será armazenado dentro da Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,19 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
+
+def process(path_file, instance: Queue):
+    data = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+
+    if new_dict not in instance.queue:
+        instance.enqueue(new_dict)
+        sys.stdout.write(f'{new_dict}')
 
 def remove(instance):
     """Aqui irá sua implementação"""
